70c54892b0d7ebef06b1129c86c847be7b7fb2b9
Jordan Wilhite <jordanwilhite@gmail.com>
2015-07-12 17:02:41 -0500

    added and validated js, but still cant get calculate button to run. not sure how to fix

--- main.js
+++ main.js
@@ -1,5 +1,32 @@
-var calculateBtn = document.getElementById('calculateBtn');
-calculateBtn.onclick = function(){
-  var sum = Number (document.getElementById('num_one').value) + Number(document.getElementById('num_two').value);
-  document.getElementById('answer').innerText = sum;
+function use strict();
+function calc() {
+var num_one;
+var num_two;
+var sign = "/";
+var answer;
+
+function getOne() {
+num_one = document.getElementById(num_one).value;
+return num_one;
+};
+
+function getTwo() {
+num_two = document.getElementById(num_two).value;
+return num_two;
+};
+
+function getSign() {
+sign = document.getElementById(sign).value;
+};
+
+function doMath() {
+var num_one = getOne();
+var num_two = getTwo();
+
+if (doMath() == "/" {
+answer = num_one / num_two;
+} else {
+answer = NaN;
+};
+};
 };
