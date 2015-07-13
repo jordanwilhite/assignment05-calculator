8dcee7a85fc6014501540a0689aede47db38192c
Jordan Wilhite <jordanwilhite@gmail.com>
2015-07-13 06:31:15 -0500

    updated indention on main.js file. going to continue to tweak code until it's due.

--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 function use strict();
-function calc() {
-var num_one;
-var num_two;
-var sign = "/";
-var answer;
+  function calc() {
+    var num_one;
+    var num_two;
+    var sign = "/";
+    var answer;
 
-function getOne() {
-num_one = document.getElementById(num_one).value;
-return num_one;
-};
+  function getOne(){
+    num_one = document.getElementById(num_one).value;
+    return num_one;
+                  };
 
-function getTwo() {
-num_two = document.getElementById(num_two).value;
-return num_two;
-};
+  function getTwo(){
+    num_two = document.getElementById(num_two).value;
+    return num_two;
+                  };
 
-function getSign() {
-sign = document.getElementById(sign).value;
-};
+  function getSign(){
+    sign = document.getElementById(sign).value;
+                    };
 
-function doMath() {
-var num_one = getOne();
-var num_two = getTwo();
+  function doMath() {
+    var num_one = getOne();
+    var num_two = getTwo();
 
-if (doMath() == "/" {
-answer = num_one / num_two;
-} else {
-answer = NaN;
-};
-};
+  if (doMath() == "/" {
+    answer = num_one / num_two};
+  else {
+    answer = NaN
+      };
+  };
 };
